a767f26218b995fa4029e2b71a5b4e00e24a6639
Ramanan Raghuraman <raghur2@sg.csc>
2025-03-02 23:15:38 +0800

    Added proper logging of the output of racadm command to a logfile Keeps track of number of racadm errors and outputs a success or failure message at the end of the configure-idrac operation Few aesthetic cleanups

--- iDRAC/iDRAC-Config-Tool.ps1
+++ iDRAC/iDRAC-Config-Tool.ps1
@@ -1,4 +1,4 @@
-# v1.0 2025-02-25
+# v1.1 2025-03-02
 # This is a script to configure iDRAC on new servers
 # Meant for SG CSC. Not for production use.
 # Created by Ramanan Raghuraman (ramanan.raghuraman@dell.com) on 2025-02-25
@@ -6,13 +6,13 @@
 # CAUTION: **** THIS SCRIPT WILL REBOOT THE SERVERS ****
 
 ##### Known issues ######
-# 1. The logging of racadm commands does not redirect error messages to the file
-# 2. The script does not alert the user if any of the racadm commands fail
-# 3. Menu items 2-4 don't work
-# 4. The confirmation display for Configure new idrac process doesn't display the idrac address correctly if there's only 1 server to be configured
-
+# 1. Menu items 2-4 don't work
 
 # Function to validate if the input is a valid IP address
+
+### Global Variables
+$logfile = "idrac-config-log.txt"
+
 function Display-Menu {
 
 	do {
@@ -79,12 +79,23 @@ function Update-Idrac {
 		[string]$pw,
 		[string]$command
 	)
+
+    $isError = 0
 	
     $fullcmd = "racadm -r $ip -u $uname -p $pw --nocertwarn $command"
     $time = Get-Date -Format "yyyy-MM-dd-HH:mm:ss"
-    "$time $fullcmd" >> idrac-config-log.txt
+    "$time $fullcmd" >> $logfile
     Write-Host $fullcmd
-    Invoke-Expression $fullcmd	*>&1 >> idrac-config-log.txt
+    Invoke-Expression "$fullcmd	2>&1 >> $logfile"
+    if ($LASTEXITCODE -ne 0) {
+        Write-Host -ForegroundColor Red "ERROR: racadm command returned error"
+        Write-Host -ForegroundColor Red "`tCommand: $fullcmd"
+        Write-Host -ForegroundColor Red "`tRefer to $logfile for details" 
+        $isError = 1
+    }
+
+    return $isError
+    
 }
 
 function Get-IdracIps {
@@ -242,7 +253,9 @@ function Configure-RootPassword {
 
     Write-Host -ForegroundColor Yellow "`Changing root password"
     #racadm -r $idracIp -u root -p $currentRootPw --nocertwarn set idrac.Users.2.Password $newPw
-    Update-Idrac -ip $idracIp -uname "root" -pw $currentPw -command "set idrac.Users.2.Password $newPw"
+    $isError = Update-Idrac -ip $idracIp -uname "root" -pw $currentPw -command "set idrac.Users.2.Password $newPw"
+
+    return $isError
 
 }
 
@@ -257,25 +270,29 @@ function Configure-DnsNtp {
         [string]$ntp2
     )
 
+    $isError = 0
+
     Write-Host -ForegroundColor Yellow "Configuring DNS ($dns1, $dns2)"
 
 	# set the DNS Server IPs
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.ipv4static.dns1 $dns1"
-	Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.ipv4static.dns2 $dns2"
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.ipv4static.dns1 $dns1")
+	$isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.ipv4static.dns2 $dns2")
 	#racadm -r $idracIp -u root -p $rootPw --nocertwarn set idrac.ipv4static.dns1 $dns1
 	#racadm -r $idracIp -u root -p $rootPw --nocertwarn set idrac.ipv4static.dns2 $dns2
 
 
     Write-Host -ForegroundColor Yellow "Configuring NTP ($ntp1, $ntp2) and Setting Timezone to Asia/Singapore"
 	# set timezone to SGT and enable NTP
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.time.timezone Asia/Singapore"
-	Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.NTPConfigGroup.NTPEnable 1"
-	Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.NTPConfigGroup.NTP1 $ntp1"
-	Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.NTPConfigGroup.NTP2 $ntp2"
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.time.timezone Asia/Singapore")
+	$isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.NTPConfigGroup.NTPEnable 1")
+	$isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.NTPConfigGroup.NTP1 $ntp1")
+	$isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.NTPConfigGroup.NTP2 $ntp2")
 	#racadm -r $idracIp -u root -p $rootPw --nocertwarn set idrac.time.timezone Asia/Singapore
 	#racadm -r $idracIp -u root -p $rootPw --nocertwarn set idrac.NTPConfigGroup.NTPEnable 1
 	#racadm -r $idracIp -u root -p $rootPw --nocertwarn set idrac.NTPConfigGroup.NTP1 $ntp1
 	#racadm -r $idracIp -u root -p $rootPw --nocertwarn set idrac.NTPConfigGroup.NTP2 $ntp2
+
+    return $isError
 }
 
 function Configure-Hostname {
@@ -300,8 +317,10 @@ function Configure-Hostname {
 	#construct the hostname and set it in iDRAC
 	$hostname = "idrac-$location-$model-node$formattedNumber-$svcTag"
     Write-Host -ForegroundColor Yellow "Setting server hostname to $hostname"
-	Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set iDRAC.NIC.DNSRacName $hostname"
+	$isError = Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set iDRAC.NIC.DNSRacName $hostname"
     #racadm -r $idracIp -u root -p $rootPw --nocertwarn set iDRAC.NIC.DNSRacName $hostname
+
+    return $isError
 }
 
 function Configure-DirectoryService {
@@ -314,37 +333,42 @@ function Configure-DirectoryService {
         [string]$server2
     )
 
+    $isError = 0
+
     Write-Host -ForegroundColor Yellow "Setting up directory service to domain $domain using $server1 and $server2"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.AuthTimeout 15"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.CertValidationEnable Disabled"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.Connection LDAPS"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.DCLookupByUserDomain Enabled"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.DCLookupDomainName $domain"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.DCLookupEnable Disabled"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.userdomain.1.name $domain"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.DomainController1 $server1"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.DomainController2 $server2"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.Enable Enabled"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.GCLookupEnable Disabled"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.GCRootDomain sg.csc"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.GlobalCatalog1 $server1"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.GlobalCatalog2 $server2"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.RacDomain $domain "
-    #Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.RSASecurID2FAAD Disabled"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.Schema 2"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.SSOEnable Disabled"
+    
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.AuthTimeout 15")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.CertValidationEnable Disabled")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.Connection LDAPS")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.DCLookupByUserDomain Enabled")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.DCLookupDomainName $domain")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.DCLookupEnable Disabled")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.userdomain.1.name $domain")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.DomainController1 $server1")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.DomainController2 $server2")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.Enable Enabled")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.GCLookupEnable Disabled")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.GCRootDomain sg.csc")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.GlobalCatalog1 $server1")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.GlobalCatalog2 $server2")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.RacDomain $domain ")
+    #$isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.RSASecurID2FAAD Disabled")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.Schema 2")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.activedirectory.SSOEnable Disabled")
 
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.1.domain sg.csc"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.1.name idrac-admin"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.1.privilege 0x1ff"
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.1.domain sg.csc")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.1.name idrac-admin")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.1.privilege 0x1ff")
 
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.2.domain sg.csc"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.2.name idrac-operator"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.2.privilege 0x0f3"
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.2.domain sg.csc")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.2.name idrac-operator")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.2.privilege 0x0f3")
 
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.3.domain sg.csc"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.3.name idrac-readonly"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.3.privilege 0x001"
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.3.domain sg.csc")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.3.name idrac-readonly")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set idrac.adgroup.3.privilege 0x001")
+
+    return $isError
 
 }
 
@@ -354,11 +378,15 @@ function Configure-BiosTimezone {
         [string]$rootPw
     )
 
+    $isError = 0
+
     Write-Host -ForegroundColor Yellow "Setting BIOS Timezone to UTC+8 on Server $idracIp"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set bios.miscsettings.timezone UTCP0800"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "jobqueue create BIOS.Setup.1-1"
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "set bios.miscsettings.timezone UTCP0800")
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "jobqueue create BIOS.Setup.1-1")
     Write-Host -ForegroundColor Yellow "Rebooting server $idracIp"
-    Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "serveraction powercycle"
+    $isError += (Update-Idrac -ip $idracIp -uname "root" -pw $rootPw -command "serveraction powercycle")
+
+    return $isError
 }
 
 
@@ -389,7 +417,11 @@ function Configure-Idrac {
     
         Write-Host "`n`n`n"
         Write-Host "The following values will be configured:"
-        Write-Host "`tServer IPs: $($idracArray[0]) ... $($idracArray[-1])"
+        Write-Host -NoNewline "`tServer IPs: "
+        foreach ($idrac in $idracArray) {
+            Write-Host -NoNewline "$idrac, "
+        }
+        Write-Host ""
         Write-Host "`tRoot password: $($rootPasswords[1])"
         Write-Host "`tHostname Prefix: $($hostnameParams[0])-$($hostnameParams[1])-node<NN>-<svctag>"
         Write-Host "`tDNS Servers: $($dnsNtpLdap[0]), $($dnsNtpLdap[1])"
@@ -399,23 +431,28 @@ function Configure-Idrac {
         $input = Read-Host "Is this correct (y/n)"
     } while (($input -ne "y") -and ($input -ne "Y")) 
 
+
+    $numErrors = 0
+
     $nodeNumber = $hostnameParams[2]
     foreach ($idrac in $idracArray) {
+        Write-Host ""
+        Write-Host ""
         Write-Host -ForegroundColor Cyan  "Starting Processing $idrac"
         Write-Host "---------------------------------------"
         Write-Host ""
         
-        Configure-RootPassword -idracIp $idrac -currentPw $rootPasswords[0] -newPw $rootPasswords[1]
+        $numErrors += Configure-RootPassword -idracIp $idrac -currentPw $rootPasswords[0] -newPw $rootPasswords[1]
         Write-Host ""
     
-        Configure-DnsNtp -idracIp $idrac -rootPw $rootPasswords[1] -dns1 $dnsNtpLdap[0] -dns2 $dnsNtpLdap[1] -ntp1 $dnsNtpLdap[2] -ntp2 $dnsNtpLdap[3]
+        $numErrors += Configure-DnsNtp -idracIp $idrac -rootPw $rootPasswords[1] -dns1 $dnsNtpLdap[0] -dns2 $dnsNtpLdap[1] -ntp1 $dnsNtpLdap[2] -ntp2 $dnsNtpLdap[3]
         Write-Host ""
     
-        Configure-Hostname -idracIp $idrac -rootPw $rootPasswords[1] -location $hostnameParams[0] -model $hostnameParams[1]  -nodeNumber $nodeNumber      
+        $numErrors += Configure-Hostname -idracIp $idrac -rootPw $rootPasswords[1] -location $hostnameParams[0] -model $hostnameParams[1]  -nodeNumber $nodeNumber      
         $nodeNumber++
         Write-Host ""
     
-        Configure-DirectoryService -idracIp $idrac -rootPw $rootPasswords[1] -domain "sg.csc" -server1 "ad-1.sg.csc" -server2 "ad-2.sg.csc"
+        $numErrors += Configure-DirectoryService -idracIp $idrac -rootPw $rootPasswords[1] -domain "sg.csc" -server1 "ad-1.sg.csc" -server2 "ad-2.sg.csc"
         Write-Host ""
     }
 
@@ -429,9 +466,27 @@ function Configure-Idrac {
     $proceed = $input.ToLower()
     if ($proceed -eq "y") {
         foreach ($idrac in $idracArray) {
-            Configure-BiosTimezone -idracIp $idrac -rootPw $rootPasswords[1]
+            $numErrors += Configure-BiosTimezone -idracIp $idrac -rootPw $rootPasswords[1]
         }
     }
+
+
+    Write-Host ""
+    Write-Host ""
+    if ($numErrors -gt 0) {
+        Write-Host -ForegroundColor Yellow "--------------------------------------------------------------------------------------------------"
+        Write-Host -ForegroundColor Yellow "# Operation completed with a total of $numErrors Error(s). Refer to $logfile for more details #"
+        Write-Host -ForegroundColor Yellow "--------------------------------------------------------------------------------------------------"
+    }
+    else {
+        Write-Host -ForegroundColor Green "-------------------------------------"
+        Write-Host -ForegroundColor Green "# Operation completed successfully! #"
+        Write-Host -ForegroundColor Green "-------------------------------------"
+    }
+
+    Write-Host
+    $null = Read-Host "Press Enter to Continue"
+    Write-Host
 
 }
 
